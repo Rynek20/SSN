d478802e9a8aa2ee97748532fa991e922f99be03
adm <adm@adm-Komputer>
2016-01-18 16:47:26 +0100

    Merge origin/master
    Conflicts:
    	src/Neuron/Neuron.java

--- src/Neuron/Neuron.java
+++ src/Neuron/Neuron.java
@@ -15,21 +15,36 @@ import java.util.Map;
 public class Neuron {
 
     private Map<String, Double> weights;
-    private int inputs
+    private int inputsAmount;
+    private int outputsAmount;
 
-    public void Neuron(int inputsNr, int outputNr) {
-        weights = new HashMap<>();
-        for (int i = 0; i < inputsNr; i++) {
-            double d =  Math.random();
-            if(d==0) d=0.1;
-            
-            weights.put("w"+i, d);
+    public void Neuron(int inputsAmount, int outputsAmount) {
+        if (inputsAmount > 0) {
+            this.inputsAmount = inputsAmount;
+        }else{
+            this.inputsAmount = 3;
+        }
+        
+        if (outputsAmount > 0) {
+            this.outputsAmount = outputsAmount;
+        } else {
+            this.outputsAmount = 1;
         }
 
-    }
-    
-    public boolean setInputSignal(double[] signals){
+        weights = new HashMap<>();
+        for (int i = 0; i < inputsAmount; i++) {
+            double d = Math.random();
+            if (d == 0) {
+                d = 0.1;
+            }
+
+            weights.put("w" + i, d);
+        }
         
+    }
+
+    public boolean setInputSignal(double[] signals) {
+        return false;
     }
 
 }
