ee47f0fd73b9bcfd80cb0d5c91c6a71ae2786ae0
adm <adm@adm-Komputer>
2016-01-20 16:44:27 +0100

    Merge origin/master
    Conflicts:
    	src/Neuron/Neuron.java

--- src/Neuron/Neuron.java
+++ src/Neuron/Neuron.java
@@ -15,16 +15,19 @@ import java.util.Map;
 public class Neuron {
 
     private Map<String, Double> weights;
+    private Map<String, Double> inputs;
     private int inputsAmount;
     private int outputsAmount;
 
     public void Neuron(int inputsAmount, int outputsAmount) {
+        inputs = new HashMap<>();
+
         if (inputsAmount > 0) {
             this.inputsAmount = inputsAmount;
-        }else{
+        } else {
             this.inputsAmount = 3;
         }
-        
+
         if (outputsAmount > 0) {
             this.outputsAmount = outputsAmount;
         } else {
@@ -33,18 +36,39 @@ public class Neuron {
 
         weights = new HashMap<>();
         for (int i = 0; i < inputsAmount; i++) {
-            double d = Math.random();
+            double d = (Math.random() * 2) - 1;
             if (d == 0) {
-                d = 0.1;
+                d = 0.01;
             }
 
             weights.put("w" + i, d);
         }
+
+    }
+
+    private double sum() {
+        double sum = 0;
+        if (inputsAmount > 0) {
+            for (int i = 0; i < inputsAmount; i++) {
+                sum += inputs.get("x"+i) * weights.get("w"+i);
+            }
+        }
+        return sum;
+    }
+    
+    private double activationFunction(double x){
         
     }
 
     public boolean setInputSignal(double[] signals) {
-        return false;
+        if (signals.length != inputsAmount) {
+            return false;
+        }
+
+        for (int i = 0; i < signals.length; i++) {
+            inputs.put("x" + i, signals[i]);
+        }
+        return true;
     }
 
 }
