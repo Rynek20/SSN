d2dc95785028cfadc28c7d0e5e65e37af5efddeb
adm <adm@adm-Komputer>
2016-01-30 12:25:21 +0100

    Merge origin/master
    Conflicts:
    	src/Neuron/Neuron.java

--- src/ssn/Network.java
+++ src/ssn/Network.java
@@ -1,55 +1,58 @@
-
 package ssn;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
+public class Network extends Thread {
 
-public class Network extends Thread{
     private int NumberOfLayers;
     private int[] NeuronsInLayer;
     private Neuron[][] networkStructure;
     private double trainingTime;
     private ArrayList<DataVector> trainingData;
     private ArrayList<DataVector> validationData;
-    
-    public Network(int NumberOfLayers, int[] NeuronsInLayer){
-        if(NeuronsInLayer.length != NumberOfLayers)
+
+    public Network(int NumberOfLayers, int[] NeuronsInLayer) {
+        if (NeuronsInLayer.length != NumberOfLayers) {
             throw new IllegalArgumentException();
-        
+        }
+
         this.NumberOfLayers = NumberOfLayers;
         this.NeuronsInLayer = NeuronsInLayer;
         this.trainingTime = -1;
         this.trainingData = new ArrayList<>();
         this.validationData = new ArrayList<>();
-        
+
         networkStructure = new Neuron[NumberOfLayers][0];
-        for(int i=0;i<NumberOfLayers;i++){
+        for (int i = 0; i < NumberOfLayers; i++) {
             networkStructure[i] = new Neuron[NeuronsInLayer[i]];
         }
-        
-        for(int i=0; i<this.NumberOfLayers;i++){
-            for(int j=0;j<this.NeuronsInLayer[i];j++){
-                int inputs=1;
-                if(i>0) inputs = this.NeuronsInLayer[i-1];
+
+        for (int i = 0; i < this.NumberOfLayers; i++) {
+            for (int j = 0; j < this.NeuronsInLayer[i]; j++) {
+                int inputs = 1;
+                if (i > 0) {
+                    inputs = this.NeuronsInLayer[i - 1];
+                }
                 networkStructure[i][j] = new Neuron(inputs);
             }
         }
-        for(int i=1; i<this.NumberOfLayers;i++){
-            for(int j=0;j<this.NeuronsInLayer[i];j++){
-                networkStructure[i][j].setInputs(networkStructure[i-1]);
+        for (int i = 1; i < this.NumberOfLayers; i++) {
+            for (int j = 0; j < this.NeuronsInLayer[i]; j++) {
+                networkStructure[i][j].setInputs(networkStructure[i - 1]);
             }
         }
     }
-    
-    public void setTrainingData(DataVector[] data){
-        
+
+    public void setTrainingData(DataVector[] data) {
+        this.trainingData.addAll(Arrays.asList(data));
     }
-    
-    public void setValidationData(DataVector[] data){
-        
+
+    public void setValidationData(DataVector[] data) {
+        this.validationData.addAll(Arrays.asList(data));
     }
-    
-    public void startTraining(){
-        
+
+    public void startTraining() {
+
     }
 }
